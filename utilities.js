426a6534e2287e0b21c73dec04073220f8bf2da3
Jiamin <jiaminzeng331698@gmail.com>
2024-01-09 14:59:05 -0500

    updated stuff

--- utilities.js
+++ utilities.js
@@ -1,9 +1,11 @@
 // initialize global variables.
-let listBox, outBox, itemSelectionBox, itemList, buyingList, changeItemBox, prevItem, prevIdx;
-// If I have time, add the text hover feature.
+let listBox, outBox, itemSelectionBox, itemList, imageSprite, imageDesc, buyingList, changeItemBox, prevItem, prevIdx;
 
+/**
+ * The Item class
+ */
 class Item {
-    constructor (name, description, image, num) {
+    constructor (name, description, image) {
         this.name = name;
         this.description = description;
         this.image = image;
@@ -16,29 +18,33 @@ function initialize() {
     listBox = document.getElementById("listbox");
     outBox = document.getElementById("outbox");
 
+    // initialize image and description
+    imageSprite = document.getElementById("itemimg");
+    imageDesc = document.getElementById("itemdesc");
+
     // initialize textboxes
     itemSelectionBox = document.getElementById("itemsel");
     changeItemBox = document.getElementById("changeitem");
 
     // initialize item list (what is available to buy)
-    itemList = [new Item("Sword of Cheese and Ants", "A sword that was wielded by cheese ants.", ""),
-    new Item ("Someone's medulla", "A part of your brainstem. Regulates heartbeat, breathing, and vomiting.", ""),
-    new Item ("Thyroid gland", "Secretes hormones TC3 and TC4.", ""),
-    new Item ("Greatsword (purple)", "It's a purple greatsword.", ""),
-    new Item ("Keys", "No one knows what these keys unlock.", ""),
-    new Item ("Panacea", "Cures all ailments and curses.", ""),
-    new Item ("Amygdala", "A part of your limbic system. Responsible for your fight-or-flight response.", ""),
-    new Item ("Moss", "Found in a prison.", ""),
-    new Item ("Shade Jacket", "You can dash through enemies with this.", ""),
-    new Item ("Hair (edible)", "If ingested, it might raise your stats.", ""),
-    new Item ("Pom", "A pom of power.", ""),
-    new Item ("Beast Eye", "A murky violet iris. Probably inedible.", ""),
-    new Item ("Chair", "It's a chair.", ""),
-    new Item ("Edible Chair", "You can eat this one.", ""),
-    new Item (".2246 lead", "What's with the precision?", ""),
-    new Item ("Feathered hat", "This hat can shoot bullets(?).", ""),
-    new Item ("Dark matter (pre-packaged)", "Does not emit or reflect light. Our universe is composed of 27% dark matter.", ""),
-    new Item ("Pulsar", "A neutron star that emits radiation from its magnetic poles.", ""),
+    itemList = [new Item("Sword of Cheese and Ants", "A sword that was wielded by cheese ants.", "cheeseSword.png"),
+    new Item ("Someone's medulla", "A part of your brainstem. Regulates heartbeat, breathing, and vomiting.", "medulla.png"),
+    new Item ("Thyroid gland", "Secretes hormones TC3 and TC4.", "thyroid.png"),
+    new Item ("Greatsword (purple)", "It's a purple greatsword.", "greatsword.png"),
+    new Item ("Keys", "No one knows what these keys unlock.", "keys.png"),
+    new Item ("Panacea", "Cures all ailments and curses.", "panacea.png"),
+    new Item ("Amygdala", "A part of your limbic system. Responsible for your fight-or-flight response.", "amygdala.png"),
+    new Item ("Moss", "Found in a prison.", "moss.png"),
+    new Item ("Shade Jacket", "You can dash through enemies with this.", "shadeJacket.png"),
+    new Item ("Hair (edible)", "If ingested, it might raise your stats.", "hat.png"), // sub
+    new Item ("Pom", "A pom of power.", "pom.png"),
+    new Item ("Beast Eye", "A murky violet iris. Probably inedible.", "beastEye.png"),
+    new Item ("Chair", "It's a chair.", "chair.png"),
+    new Item ("Edible Chair", "You can eat this one.", "edibleChair.png"),
+    new Item (".2246 lead", "What's with the precision?", "lead.png"),
+    new Item ("Feathered hat", "This hat can shoot bullets(?).", "hat.png"),
+    new Item ("Dark matter (pre-packaged)", "Does not emit or reflect light. Our universe is composed of 27% dark matter.", "darkMatter.png"),
+    new Item ("Pulsar", "A neutron star that emits radiation from its magnetic poles.", "hat.png"), // sub
     new Item ("Milk", "Someone's dad left it here.", "")
     ];
 
@@ -49,6 +55,7 @@ function initialize() {
 }
 
 const display = () => {
+    // show image and item description
     listBox.innerHTML = "0: " + itemList[0].name;
     for (let i = 1; i < itemList.length; i++) {
         listBox.innerHTML += "<br />" + i + ": " + itemList[i].name;
@@ -71,6 +78,8 @@ const deleteAdd = (num, change) => {
     if (isNaN(num) || num < 0) {
         return;
     }
+    changeImg(num);
+
     switch (change) {
         case 0:
             if (num < itemList.length) {
@@ -118,4 +127,9 @@ const changeItem = (num, change) => {
         [buyingList[num], buyingList[swapIndex]] = [buyingList[swapIndex], prevItem];
     }
     display();
+}
+
+const changeImg = (itemSelection) => {
+    imageSprite.src = itemList[itemSelection].image;
+    imageDesc.innerHTML = itemList[itemSelection].description;
 }
